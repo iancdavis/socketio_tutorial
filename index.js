e26c7bbaedf3809b6bc5ab3a66cc4eecc8c5d30c
Ian Davis <iancdavis7@gmail.com>
2020-10-30 09:09:00 -0600

    second first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')()
+var http = require('http').createServer(app)
+
+/* app.get('/', (req, res) => {
+    res.send('<h1>Hello World, whats up</h1>')
+}) */
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+http.listen(3000, () => {
+    console.log('listening on *:3000')
+})

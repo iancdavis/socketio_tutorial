987d124cdc74a9c287625eb34d64d0c116a8873a
Ian Davis <iancdavis7@gmail.com>
2020-11-02 16:05:14 -0700

    finished tutorial

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')()
 var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
 /* app.get('/', (req, res) => {
     res.send('<h1>Hello World, whats up</h1>')
@@ -7,6 +8,12 @@ var http = require('http').createServer(app)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
 })
 
 http.listen(3000, () => {
